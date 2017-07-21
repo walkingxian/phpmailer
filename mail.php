e5afe6755be8ffd9811d08fcb5eeaf93cb691566
小贤 <527723190@qq.com>
2017-07-21 10:59:37 +0800

    测试phpmailer

--- mail.php
+++ mail.php
@@ -4,7 +4,7 @@
  * @Date:   2017-07-21 09:38:51
  * @Last Modified by:   Marte
  * @Last Modified time: 2017-07-21 10:41:48
- */
+ */ 
 header('content-type:text/html;charset=utf-8');
 require_once("./phpmail/functions.php");
 $flag = sendMail('example@163.com','邮件测试','恭喜你测试成功，开启你的学习之旅吧！');
