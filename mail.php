2108604526e5b83ac656de1c93241762af3ba97b
527723190@qq.com <527723190@qq.com>
2017-07-21 10:56:19 +0800

    phpmailer

---
+++ mail.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @Author: Marte
+ * @Date:   2017-07-21 09:38:51
+ * @Last Modified by:   Marte
+ * @Last Modified time: 2017-07-21 10:41:48
+ */
+header('content-type:text/html;charset=utf-8');
+require_once("./phpmail/functions.php");
+$flag = sendMail('example@163.com','邮件测试','恭喜你测试成功，开启你的学习之旅吧！');
+if($flag){
+    echo "发送邮件成功！";
+}else{
+    echo "发送邮件失败！";
+}
+?>
